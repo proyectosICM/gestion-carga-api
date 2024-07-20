2442a8bbf952caad0458a10bd5bb8224f3cb9daa
Eduardo Aguilar <es123y123@gmail.com>
2024-07-19 19:41:10 -0500

    ccc

--- src/main/java/com/icm/gestioncargaapi/security/WebSecurityConfig.java
+++ src/main/java/com/icm/gestioncargaapi/security/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://samloto.com:4003"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
